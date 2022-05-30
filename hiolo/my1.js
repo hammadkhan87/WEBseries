5d7164de844aa986481adde35cfc2ab9031bc6ff
hammadkhan <86566345+hammadkhan87@users.noreply.github.com>
2022-05-30 07:37:34 +0500

    Add files via upload

---
+++ hiolo/my1.js
@@ -0,0 +1,9 @@
+var element;
+element=document.getElementById("abc").getAttribute("name");
+document.getElementById("abc").innerHTML="<h1>thw</h1>";
+var ele=document.querySelector("#abc").style.color;
+document.querySelector("#abc").style.backgroundColor="red"
+document.querySelector("#abc").className="chek";
+eleb=document.querySelector("#abc").className;
+
+console.log(eleb);
